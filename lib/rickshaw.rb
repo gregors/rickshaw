21516a9265f21980bfdc5584eeb7b97a0be0106a
Gregory Ostermayr <greg@carbonfive.com>
2014-10-15 20:09:48 -0400

    add md5

--- lib/rickshaw.rb
+++ lib/rickshaw.rb
@@ -1,10 +1,25 @@
+require 'digest/md5'
 require 'digest/sha1'
 require 'digest/sha2'
 require 'rickshaw/version'
+require 'core_ext/string/to_md5'
 require 'core_ext/string/to_sha1'
 require 'core_ext/string/to_sha256'
 
 module Rickshaw
+  module MD5
+    def self.hash(file_path)
+      hash = Digest::MD5.new
+      open(file_path, 'r') do |io|
+        until io.eof?
+          buffer = io.read(1024)
+          hash.update(buffer)
+        end
+      end
+      hash.hexdigest
+    end
+  end
+
   module SHA1
     def self.hash(file_path)
       hash = Digest::SHA1.new
